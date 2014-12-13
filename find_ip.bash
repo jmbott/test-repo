36a01964e89669247135a904bad06372d6905ed2
Jack Bott <millerbott@gmail.com>
2014-12-13 14:57:59 -0500

    added script

---
+++ find_ip.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# script to find all IPs on network that respond to pings
+# to run $ sudo bash find_ip.bash
+
+time ( s=192.168.1 ; for i in $(seq 1 254) ; do ( ping -n -c 1 -w 1 $s.$i 1>/dev/null 2>&1 && printf "%-16s %s\n" $s.$i responded ) & done ; wait ; echo )
