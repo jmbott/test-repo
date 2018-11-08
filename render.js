547ee7671e2648ef0541dd9e4af91fa5f8ce98a1
jmbott <millerbott@gmail.com>
2018-11-08 08:50:35 -0500

    Update render.js

--- render.js
+++ render.js
@@ -8,7 +8,7 @@ page.viewportSize = { width: 1024, height: 768 };
 //the clipRect is the portion of the page you are taking a screenshot of
 page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
 //the rest of the code is the same as the previous example
-page.open('https://www.cs.columbia.edu/~rgu/courses/e6998/fall2018/Lecture0.html', function() {
+page.open('https://www.example.com/index.html', function() {
   page.render('out.pdf');
   phantom.exit();
 });
